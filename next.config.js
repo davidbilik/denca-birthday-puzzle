d591939eb1f2a86b114ff4230e7ab1f9ccb3f470
David Bilik <david.bilik@ackee.cz>
2024-12-29 09:22:17 +0100

    Prepare for deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // This disables strict mode
-};
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+}
 
-module.exports = nextConfig; 
+module.exports = nextConfig 
