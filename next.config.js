16d633b9602c1cb57dc60c9b6b0cbc3ddf604aea
David Bilik <david.bilik@ackee.cz>
2024-12-29 10:01:21 +0100

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let basePath = ''
-if (isGithubActions) {
-  // Trim off `<owner>/` from the repo name
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
   },
-  basePath,
-  // Make sure assets are looked up relative to the base path
-  assetPrefix: basePath,
+  // Base path will be automatically injected by actions/configure-pages@v5
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 module.exports = nextConfig 
