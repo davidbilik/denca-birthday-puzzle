d66e62f7c9453eb23fca6302cd56e9883195284f
David Bilik <david.bilik@ackee.cz>
2024-12-29 09:49:09 +0100

    Refactor  GH action

--- next.config.js
+++ next.config.js
@@ -1,39 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  // This ensures that static files are served correctly
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(mp3|wav)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: `${assetPrefix}_next/static/media`,
-            outputPath: 'static/media',
-            name: '[name].[hash].[ext]',
-          },
-        },
-      ],
-    });
-    return config;
-  },
+  // Base path will be automatically injected by actions/configure-pages@v5
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 module.exports = nextConfig 
